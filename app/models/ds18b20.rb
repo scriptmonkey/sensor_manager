4c5f3573ea497b5fba29cf7706e26f3b96c0d9ad
Rod Jenkins <rodj@rodandmichelle.com>
2017-04-26 16:12:54 -0500

    added the reading of a proper ds18b20 sensor file

--- app/models/ds18b20.rb
+++ app/models/ds18b20.rb
@@ -4,20 +4,32 @@ class Ds18b20 < ApplicationRecord
     with: /\/\z/,
     message: 'Path must end with a /'
   }
-  def creading
+  def read_file
     if File.exist?(path + file)
-      File.read(path + file).chomp
+      sensor = File.read(path + file).chomp
     else
-      '-256'
+      return -256
     end
+
+    lines = sensor.lines
+    if lines[0].include? 'YES'
+      string_temp = lines[1].split('=')[1]
+      string_temp.to_f/1000
+    else
+      return -255
+    end
+
+  end
+  def creading
+    read_file
   end
 
   def freading
-    if creading.to_i <= -250
-      '-256'
+    if creading <= -250
+      creading 
     else
-      ctemp=creading.to_i*1.8+32
-      ctemp.to_i.to_s
+      ctemp=creading.to_f*1.8+32
+      ctemp.to_s
     end
   end
 end
--- spec/models/ds18b20_spec.rb
+++ spec/models/ds18b20_spec.rb
@@ -6,21 +6,41 @@ RSpec.describe Ds18b20, type: :model do
   it {should respond_to :file}
   it {should respond_to :creading }
   it {should respond_to :freading }
+  it {should respond_to :read_file }
 
   context 'has a file to read' do
-
-    it 'expects a temp reading' do
-      expect(build(:ds18b20).creading).to eq('69')
-      expect(build(:ds18b20).freading).to eq('156')
+    it 'expects a temp reading from sensor1.txt' do
+      expect(build(:ds18b20).creading).to eq(69.0)
+      expect(build(:ds18b20).freading).to eq('156.2')
     end
+
+    it 'expects a temp reading from sensor2.txt' do
+      expect(build(:ds18b20, file: 'sensor2.txt').creading).to eq(10.375)
+      expect(build(:ds18b20).freading).to eq('156.2')
+    end
+
+    it 'read_file should return a celsius temp' do
+      expect(build(:ds18b20).creading).to eq(69.0)
+    end
+
   end
 
   context 'does not have a file to read' do
     it 'expects an error reading' do
       sensor = build(:ds18b20)
       sensor.file = 'bad file'
-      expect(sensor.creading).to eq('-256')
-      expect(sensor.freading).to eq('-256')
+      expect(sensor.creading).to eq(-256)
+      expect(sensor.freading).to eq(-256)
+    end
+  end
+
+  context 'sensor reports bad CRC' do
+    it 'expects an error reading' do
+      sensor = build(:ds18b20)
+      sensor.file = 'sensor_bad_crc.txt'
+      #sensor.file = 'sensor_real.txt'
+      expect(sensor.creading).to eq(-255)
+      expect(sensor.freading).to eq(-255)
     end
   end
 
